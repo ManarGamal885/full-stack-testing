315be40e81c574f7a64f500c768ec4c57659953d
ManarGamal885 <manarg281@gmail.com>
2025-06-17 23:43:49 +0300

    Adding test files for wireMock .

---
+++ full_stack_testing/src/main/java/chapters/chapter2ManualExploratoryTesting/wireMock.java
@@ -0,0 +1,9 @@
+package chapters.chapter2ManualExploratoryTesting;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+public class wireMock {
+    public static void main(String[] args) {
+        System.out.println("kk");
+    }
+}
---
+++ full_stack_testing/src/test/java/chapter2ManualExploratoryTesting/wireMock.java
@@ -0,0 +1,40 @@
+package chapter2ManualExploratoryTesting;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class wireMock extends chapter2Setup {
+    private static final String resourceEndPoint = "/my/resource";
+    private static final String resourceBody = "<response>SUCCESS</response>";
+
+    @Test
+    public void testPostResource() throws IOException, InterruptedException {
+        // Stub POST endpoint
+        stubFor(post(resourceEndPoint)
+                .withHeader("Content-Type", containing("xml"))
+                .willReturn(ok()
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody(resourceBody)));
+
+        // Build POST request
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(wireMockServer.url(resourceEndPoint)))
+                .header("Content-Type", "text/xml")
+                .POST(HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        // Send request and capture response
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Assertions
+        Assert.assertEquals(response.statusCode(), 200, "Wrong response status code");
+        Assert.assertTrue(response.body().contains(resourceBody), "Wrong response body");
+    }
+}
