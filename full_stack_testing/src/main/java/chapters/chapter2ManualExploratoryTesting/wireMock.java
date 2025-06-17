adc6761f6c1079836580d978f7b3d6f2d3a421d0
ManarGamal885 <manarg281@gmail.com>
2025-06-17 23:45:47 +0300

    Refactor test files for wireMock .

--- full_stack_testing/src/main/java/chapters/chapter2ManualExploratoryTesting/wireMock.java
+++
@@ -1,9 +0,0 @@
-package chapters.chapter2ManualExploratoryTesting;
-
-import com.github.tomakehurst.wiremock.WireMockServer;
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-public class wireMock {
-    public static void main(String[] args) {
-        System.out.println("kk");
-    }
-}
